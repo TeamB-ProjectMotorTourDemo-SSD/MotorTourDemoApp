bf19aff682073c9ece2316624a5b5302aada66ee
Nuzrah <nuzrah.nilamdeen@gmail.com>
2020-05-27 19:34:25 +0530

    Resolved conflicts - master ready

--- src/main/java/com/nuzrah/projectmotortourdemo/dao/TOUR_AVAILABILITY.java
+++ src/main/java/com/nuzrah/projectmotortourdemo/dao/TOUR_AVAILABILITY.java
@@ -1,29 +1,7 @@
 package com.nuzrah.projectmotortourdemo.dao;
 
 public enum TOUR_AVAILABILITY {
-<<<<<<< HEAD
-	ONGOING("ONGOING"),
-	UPCOMING("UPCOMING"),
-	COMPLETED("COMPLETED");
-	
-	
-	private String name;
 
-    private TOUR_AVAILABILITY(String stringVal) {
-        name=stringVal;
-    }
-    public String toString(){
-        return name;
-    }
-
-    public static String getEnumByString(String code){
-        for(TOUR_AVAILABILITY e : TOUR_AVAILABILITY.values()){
-            if(e.name.equals(code)) return e.name();
-        }
-        return null;
-    }
-	
-=======
 	ONGOING("Completed Tour"),
 	UPCOMING("On-going Tour"),
 	COMPLETED("Upcoming Tour");
@@ -43,5 +21,4 @@ public enum TOUR_AVAILABILITY {
         }
         return null;
     }
->>>>>>> branch 'Version_4_Merge' of https://github.com/TeamB-ProjectMotorTourDemo-SSD/MotorTourDemoApp.git
 }
